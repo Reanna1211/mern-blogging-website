98c638218f6ac16611950ff5729a338ec1d25d9c
Reanna Musoke <reannamusoke94@gmail.com>
2025-04-05 16:30:12 +0100

    Add navbar to app.jsx

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Navbar from "./components/navbar.component"
 
 const App = () => {
   return (
-    <div>
-      <h1>MERN Blogging Website by Modern Web</h1>
-    </div>
+    <Navbar />
   );
 };
 
