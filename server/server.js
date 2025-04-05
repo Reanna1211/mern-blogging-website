20aec3c66a1bcc2e2adb0acc758f3c8b44248628
Reanna Musoke <reannamusoke94@gmail.com>
2025-04-05 14:26:38 +0100

    Update server configuration and fix port issue

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
-const PORT = process.env.PORT || 5000; // Default to 5000 locally, use Heroku's dynamic port in production
+const PORT = process.env.PORT || 5001;; // Default to 5001 locally, use Heroku's dynamic port in production
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
